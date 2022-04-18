2781a100c274fa1ea4b0f8ae77b421fa26e81c89
DarshaniKaushalya <darshanikaushalya7788dkd@gmail.com>
2022-04-18 21:06:56 +0530

    feat:Applying dark mode

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -2,24 +2,33 @@ import React from 'react';
 import{AiFillGithub, AiFillLinkedin,AiFillYoutube} from 'react-icons/ai';
 import {GoLocation} from 'react-icons/go';
 import {GiTie} from 'react-icons/gi'
+import {useTheme} from 'next-themes'
 
 const Sidebar = () => {
+
+    
+    const {theme,setTheme} = useTheme();
+
+    const changeTheme = ()=>{
+        setTheme(theme==="light" ? "dark":"light");
+    }
+    
   return (
     <div><img 
     src='/images/kau.jpg' 
     alt='user avatar'
-    className='w-32 h32 mx-auto rounded-full'
+    className='w-32 mx-auto rounded-full h32'
     />
     <h3 className='my-4 text-3xl font-medium tracking-wider font-kaushan'>
         <span className='text-green'>Darshani </span> 
         Kaushalya
     </h3>
-    <p className='px-2 py-1 my-3 bg-gray-200 rounded-full'>Web Developer</p>
-    <a className='px-2 py-1 my-3 bg-gray-200 rounded-full flex items-center justify-center' 
+    <p className='px-2 py-1 my-3 bg-gray-200 rounded-full dark:bg-dark-200'>Web Developer</p>
+    <a className='flex items-center justify-center px-2 py-1 my-3 bg-gray-200 rounded-full dark:bg-dark-200' 
         href='' 
         download="name"><GiTie className='w-6 h-6'/>Download Resume</a>
  {/* Social Icons */}
-<div className='flex justify-around my-5 text-green md:w-full mx-auto'>
+<div className='flex justify-around mx-auto my-5 text-green md:w-full'>
     <a href=''>
         <AiFillGithub className='w-8 h-8 cursor-pointer'/>
     </a>
@@ -31,7 +40,7 @@ const Sidebar = () => {
     </a>
 </div>
  {/* Address */}
- <div className='my-5 py-4 bg-gray-200' style={{marginLeft:'-1rem',marginRight:'-1rem'}}>
+ <div className='py-4 my-5 bg-gray-200 dark:bg-dark-200' style={{marginLeft:'-1rem',marginRight:'-1rem'}}>
      <div className='flex items-center justify-center space-x-2'>
          <GoLocation/>
          <span>Wariyapola, Srilanka</span>
@@ -40,11 +49,11 @@ const Sidebar = () => {
      <p className='my-2'>1234567890</p>
  </div>
  {/* Email Button */}
- <button className="bg-gradient-to-r from-green to-blue-400 w-8/12 rounded-full py-2 px-5 text-white my-2
- focus:outline-none
- " 
+ <button className="w-8/12 px-5 py-2 my-2 text-white rounded-full bg-gradient-to-r from-green to-blue-400 focus:outline-none dark:bg-dark-200" 
  onClick={()=>window.open('mailto:darshanikaushalya7788dkd@gmail.com')}>Email Me</button>
- <button className="bg-gradient-to-r from-green to-blue-400 w-8/12 rounded-full py-2 px-5 text-white my-2">Toggle Theme</button>
+ <button 
+ onClick={changeTheme}
+ className="w-8/12 px-5 py-2 my-2 text-white rounded-full bg-gradient-to-r from-green to-blue-400">Toggle Theme</button>
 
     </div>
   )
